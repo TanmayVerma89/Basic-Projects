8ce67e5523e63580d6acc05052deda34a0e54a2f
TanmayVerma89 <TanmayVerma566@gmail.com>
2026-03-24 00:58:14 +0530

    Boilerplate and Dock

---
+++ Mac-OS/src/components/Dock.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './dock.scss';
+
+const Dock = () => {
+  return (
+    <footer className='dock'>
+        <div className="icon github"><img src="./dock-icons/github.svg" alt="icons" /></div>
+        <div className="icon calendar"><img src="./dock-icons/calender.svg" alt="icons" /></div>
+        <div className="icon spotify"><img src="./dock-icons/spotify.svg" alt="icons" /></div>
+        <div className="icon pdf"><img src="./dock-icons/pdf.svg" alt="icons" /></div>
+        <div className="icon note"><img src="./dock-icons/note.svg" alt="icons" /></div>
+        <div className="icon mail"><img src="./dock-icons/mail.svg" alt="icons" /></div>
+        <div className="icon link"><img src="./dock-icons/link.svg" alt="icons" /></div>
+        <div className="icon cli"><img src="./dock-icons/cli.svg" alt="icons" /></div>
+    </footer>
+  )
+}
+
+export default Dock
